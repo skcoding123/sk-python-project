1a5a4b30cec121b6cfedd8600cafc3c441414ef1
Sunil Maharana <140019836+skcoding123@users.noreply.github.com>
2025-01-08 13:04:18 +0400

    Update math.py

--- math.py
+++ math.py
@@ -1,1 +1,14 @@
+#import math   (import this module for mathematics operation)
+#math.sqrt(X)...square root of X   |   math.ceil(x)...round a no up   | math.floor(x)...round a no down
+
+x = 9.9
+
+print(math.pi)
+print(math.e)
+result = math.sqrt(x)
+result = math.ceil(x)
+result = math.floor(x)
+
+print(result)
+
 
