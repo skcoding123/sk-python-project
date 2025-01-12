979f2e75fa0781560b678bb9cd2408464f2d237c
Sunil Maharana <140019836+skcoding123@users.noreply.github.com>
2025-01-12 23:45:37 +0400

    Update python-calculator.py

--- python-calculator.py
+++ python-calculator.py
@@ -1,1 +1,27 @@
+#python calculator
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter number 1: "))
+num2 = float(input("Enter number 2: "))
 
+if operator == "+":
+  result = num1 + num2
+  print(f"The result is : {result}")
+elif operator == "-":
+  result = num1 - num2
+  print(f"The result is : {result}")
+
+elif operator == "*":
+  result = num1 * num2
+  print(f"The result is : {result}")
+
+elif operator == "/":
+  result = num1 / num2
+  print(f"The result is : {result}")
+
+else:
+  print(f"The {operator} is not valid operator")
+
+
+
+
+  
