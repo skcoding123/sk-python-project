151ccfa3bfef32c4f4188d68e77e3d3f54811db0
skcoding123 <sunilkumar.maharana36@gmail.com>
2025-01-12 15:43:11 +0400

    added program

--- math-sk.py
+++ math-sk.py
@@ -23,11 +23,20 @@
 
 # Area of a circle A = pie r*2
 
-import math
+#import math
 
 radius = float(input("Enter the radius of a circle: "))
-area = math.pi * pow(radius, 2)
-print(f"The area of the circle is: {area}")
+#area = math.pi * pow(radius, 2)
+#print(f"The area of the circle is: {area}")
+#print(f"The area of the circle is: {round (area, 2)} cm")
+
+#hypotenuse of a triangle (c = sqrt of a*2 + b*2)
+#import math
+#a = float(input("Enter side A: "))
+#b = float(input("Enter side B: "))
+#c = math.sqrt(pow(a,2) + pow(b,2))
+
+#print(f"Side c: {c}")
 
 
 
